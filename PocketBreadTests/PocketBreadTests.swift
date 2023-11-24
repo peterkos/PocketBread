5fecc3f6a177ca35ab275b836731ac9372b9cd59
Peter Kos <pkos91@icloud.com>
2023-11-24 16:48:22 -0500

    add swiftformat

--- PocketBreadTests/PocketBreadTests.swift
+++ PocketBreadTests/PocketBreadTests.swift
@@ -5,11 +5,10 @@
 //  Created by Peter Kos on 11/24/23.
 //
 
-import XCTest
 @testable import PocketBread
+import XCTest
 
 final class PocketBreadTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class PocketBreadTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
