22f35d6eeefb430dbdd7f1a51075ca745bc71e45
Peter Kos <pkos91@icloud.com>
2023-11-24 16:46:27 -0500

    Initial Commit

---
+++ PocketBread/PocketBreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketBreadApp.swift
+//  PocketBread
+//
+//  Created by Peter Kos on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PocketBreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
